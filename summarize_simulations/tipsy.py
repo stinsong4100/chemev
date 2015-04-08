9420b57ab62f912d232e3275ea5856e14d5f7316
Greg Stinson <stinsongr@gmail.com>
2015-04-08 19:04:06 +0200

    debugged tipsy sim summary and added pickle file

--- summarize_simulations/tipsy.py
+++ summarize_simulations/tipsy.py
@@ -11,11 +11,12 @@ sim_time=13.73e9
 step_time=1e6
 
 # Star formation history from starlog
-sl = pynbody.StarLog('g1536.starlog')
+sl = pynbody.tipsy.StarLog('g1536.starlog')
 sfh,startimes = np.histogram(sl['tform'].in_units('Gyr'),weights=sl['massform'].in_units('Msol'),
                              bins=sim_time/step_time)
 
 gastimes=[]
+gasmass=[]
 # Gas Disk Mass evolution from data files
 dfs = glob.glob('/disk2/stinson/g1536/changa/esn1.5/00????/g1536.00????.data')
 for df in dfs:
