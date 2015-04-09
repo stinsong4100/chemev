61860af0dd872a6ee8d530833076cfdc8758d558
Greg Stinson <stinsongr@gmail.com>
2015-04-09 22:19:22 +0200

    adding iwamoto and karakas pickling scripts

---
+++ yield_data/iwamoto99/pickle_iwamoto.py
@@ -0,0 +1,19 @@
+import numpy as np, pickle, pdb
+
+isotope_yield={}
+element_yield={}
+infile='iwamoto99.txt'
+
+prods = np.genfromtxt(infile,usecols=(2),skip_header=2)
+isos = np.genfromtxt(infile,usecols=(0),dtype='string',skip_header=2)
+
+for ii,iso in enumerate(isos):
+    ciso = '^'+iso[:2]+'^'+iso[2:]
+    isotope_yield[ciso] = prods[ii]
+        
+for iso in isotope_yield.keys():
+    element = iso[2:]
+    element_yield[element] = isotope_yield[iso]
+
+pickle.dump({'element_yield':element_yield,'isotope_yield':isotope_yield},
+            open('iwamoto99sniaW7.pck','w'))
--- yield_data/karakas10/pickle_karakas.py
+++ yield_data/karakas10/pickle_karakas.py
@@ -1,21 +1,24 @@
 import numpy as np, pickle, pdb
 
-infiles = ['nonrotateZ0.txt','nonrotateZsol.txt']
-Zs = [0,1]
-masses = [13,15,20,25,30,35,50,80]
+infiles = ['Z_0.0001.txt','Z_0.004.txt','Z_0.008.txt','Z_0.02.txt']
+Zs = [1e-4,4e-3,8e-3,0.02]
 
 # load decay products
-pre,post = np.genfromtxt('decay.txt',skip_header=1,dtype='string',unpack=True)
+pre,post = np.genfromtxt('../general/decay_products.txt',skip_header=1,dtype='string',unpack=True)
 decay_products = {}
 for ip,pre in enumerate(pre): decay_products[pre]=post[ip]
 
 isotope_yield={}
 element_yield={}
 
+el_trans = {'P':'^1^H','D':'^2^H','He3':'^3^He','He4':'^4^He','Li7':'^7^Li',
+            'Be7':'^7^Be','B8':'^8^B'}
+
 for iz,infile in enumerate(infiles):
     
     prods = np.genfromtxt(infile,usecols=np.arange(1,len(masses)+1))
-    isos = np.genfromtxt(infile,usecols=(0),dtype='string')
+    isos = np.genfromtxt(infile,usecols=(3),dtype='string').capitalize()
+    masses = np.unique(np.genfromtxt(infile,usecols=(0)))
     
     for ii,iso in enumerate(isos):
         if iso not in decay_products: continue
