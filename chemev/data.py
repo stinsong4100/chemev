d5941e3aec1ef02555cf3c60dbc3d930793f9e12
Greg Stinson <stinsongr@gmail.com>
2015-04-16 18:45:04 +0200

    changed data from objects to dictionarys for generality

--- chemev/data.py
+++ chemev/data.py
@@ -1,27 +1,31 @@
 import pickle, numpy as np
 
 # Read in chemical evolution tables
-global agb_dat , iagbAgeSort, AGBageIndices, iagbZsort, AGBZindices
+global tables, lists
+tables = {}
+lists = {}
 try:
-    agb_dat = pickle.load(open('agb_enrich.pck'))
+    tables['agb'] = pickle.load(open('agb_enrich.pck'))
 except:
     print "Couldn't open agb_enrich.pck"
     pass
-iagbAgeSort = np.argsort(agb_dat['times'][0])
-AGBageIndices = np.arange(len(agb_dat['times'][0]))
-iagbZsort = np.argsort(agb_dat['Zs'])
-AGBZindices = np.arange(len(agb_dat['Zs']))
+lists['agb'] = {}
+lists['agb']['iAgeSort'] = np.argsort(tables['agb']['times'][0])
+lists['agb']['ageIndices'] = np.arange(len(tables['agb']['times'][0]))
+lists['agb']['iZsort'] = np.argsort(tables['agb']['Zs'])
+lists['agb']['Zindices'] = np.arange(len(tables['agb']['Zs']))
 
 global snii_dat, isnAgeSort, SNageIndices, isnZsort, SNZindices
 try:
-    snii_dat = pickle.load(open('snii_enrich.pck'))
+    tables['snii'] = pickle.load(open('snii_enrich.pck'))
 except:
     print "Couldn't open snii_enrich.pck"
     pass
-isnAgeSort = np.argsort(snii_dat['times'][0])
-SNageIndices = np.arange(len(snii_dat['times'][0]))
-isnZsort = np.argsort(snii_dat['Zs'])
-SNZindices = np.arange(len(snii_dat['Zs']))
+lists['snii'] = {}
+lists['snii']['iAgeSort'] = np.argsort(tables['snii']['times'][0])
+lists['snii']['ageIndices'] = np.arange(len(tables['snii']['times'][0]))
+lists['snii']['iZsort'] = np.argsort(tables['snii']['Zs'])
+lists['snii']['Zindices'] = np.arange(len(tables['snii']['Zs']))
 
 global star_type
 star_type = np.dtype({'names':['tform','init_mass','mass','Z'],
