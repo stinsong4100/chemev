0b5075f52a612008afb3ed9f1a201c9d525e5611
Greg Stinson <stinsongr@gmail.com>
2015-04-29 16:51:12 +0200

    read WW95 into generic format almost, masses are still string because some masses have A, B, and C models representing different energies.  still need to split those off

--- yield_data/ww95/toarray.py
+++ yield_data/ww95/toarray.py
@@ -1,8 +1,10 @@
 import numpy as np, pickle, pdb
 
-infiles = ['Z0.txt','Z1e-4sol.txt','Z0.01.txt','Z0.1.txt','Zsol.txt']
+infiles = ['Z0.txt','Z1e-4sol.txt','Z0.01.txt','Z0.1sol.txt','Zsol.txt']
 Zs = [0,2e-6,2e-4,2e-3,0.02]
-masses = [11,12,13,15,18,19,20,22,25,30,30,35,35,35,40,40]
+mass_names = ('11A','12A','13A','15A','18A','19A','20A','22A','25A','25B','30A','30B','35A','35B','35C','40A','40B','40C')
+formats= (len(mass_names)*['f'])
+mass_type = np.dtype({'names':mass_names,'formats':formats})
 
 # load decay products
 pre,post = np.genfromtxt('../general/decay_products.txt',skip_header=1,dtype='string',unpack=True)
@@ -13,28 +15,39 @@ isotope_yield={}
 element_yield={}
 
 el_trans = {'1H':'^1^H','2H':'^2^H','3He':'^3^He','4He':'^4^He','7Li':'^7^Li',
-            '7Be':'^7^Be','9Be':'^9^Be'}
+            '7Be':'^7^Be','9Be':'^9^Be','m_ej':'m_ej'}
+isos = np.genfromtxt('isonames.txt',usecols=(0),dtype='string')
 
 for iz,infile in enumerate(infiles):
     
-    prods = np.genfromtxt(infile,usecols=np.arange(1,len(masses)+1),names=True)
-#,names=['%d m'%m for m in masses])
-    isos = np.genfromtxt(infile,usecols=(0),dtype='string')
+    prods = np.genfromtxt(infile,names=True)
     
     for ii,iso in enumerate(isos):
         if iso in el_trans.keys(): ciso = el_trans[iso]
-        else: 
-            ciso = '^'+iso[-2:]+'^'+iso[:-2]
+        else:  ciso = '^'+iso[:2]+'^'+iso[2:]
 
-        if ciso not in decay_products: continue
+        if ciso not in decay_products: 
+            continue
         if decay_products[ciso] not in isotope_yield: 
-            isotope_yield[decay_products[ciso]] = np.zeros((len(Zs),len(masses)))
-        isotope_yield[decay_products[ciso]][iz,:] += prods[ii]
+            isotope_yield[decay_products[ciso]] = np.zeros((len(Zs)),dtype=mass_type)
+        for m in prods.dtype.fields.keys():
+            isotope_yield[decay_products[ciso]][m][iz] += prods[m][ii]
+    if 'm_ej' not in isotope_yield: 
+        isotope_yield['m_ej'] = np.zeros((len(Zs)),dtype=mass_type)
+    for m in prods.dtype.fields.keys():
+        isotope_yield['m_ej'][m][iz] += prods[m][ii-1]
         
     for iso in isotope_yield.keys():
         element = iso.split('^')[-1]
-        if iz==0: element_yield[element] = np.zeros((len(Zs),len(masses)))
-        element_yield[element] += isotope_yield[iso][iz,:]
+        if iz==0: element_yield[element] = np.zeros((len(Zs)),dtype=mass_type)
+        for m in mass_names:
+            element_yield[element][m][iz] += isotope_yield[iso][m][iz]
+    if 'm_ej' not in element_yield: 
+        element_yield['m_ej'] = np.zeros((len(Zs)),dtype=mass_type)
+    for m in prods.dtype.fields.keys():
+        element_yield['m_ej'][m][iz] += prods[m][ii-1]
 
+    
+pdb.set_trace()
 pickle.dump({'isotope_yield':isotope_yield,'element_yield':element_yield,
-             'masses':masses,'Zs':Zs},open('ww95.pck','w'))
+             'masses':mass_names,'Zs':np.array(Zs)},open('ww95.pck','w'))
