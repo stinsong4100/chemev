390164dfdcc7d761d40c50fc089c2b4c30d5a04e
Greg Stinson <stinsongr@gmail.com>
2015-05-04 18:38:17 +0200

    debugged toarrary and added interpolation/exptrapolation so WW tables are getting filled reasonably well, there are still some nans floating around

--- yield_data/ww95/toarray.py
+++ yield_data/ww95/toarray.py
@@ -2,9 +2,11 @@ import numpy as np, pickle, pdb
 
 infiles = ['Z0.txt','Z1e-4sol.txt','Z0.01.txt','Z0.1sol.txt','Zsol.txt']
 Zs = [0,2e-6,2e-4,2e-3,0.02]
-mass_names = ('11A','12A','13A','15A','18A','19A','20A','22A','25A','25B','30A','30B','35A','35B','35C','40A','40B','40C')
-formats= (len(mass_names)*['f'])
-mass_type = np.dtype({'names':mass_names,'formats':formats})
+#mass_names = ('11A','12A','13A','15A','18A','19A','20A','22A','25A','25B','30A','30B','35A','35B','35C','40A','40B','40C')
+#formats= (len(mass_names)*['f'])
+#mass_type = np.dtype({'names':mass_names,'formats':formats})
+masses = np.array([11,12,13,15,18,19,20,22,25,30,35,40])
+models = ['A','A','A','A','A','A','A','A','A','B','C','C']
 
 # load decay products
 pre,post = np.genfromtxt('../general/decay_products.txt',skip_header=1,dtype='string',unpack=True)
@@ -25,29 +27,51 @@ for iz,infile in enumerate(infiles):
     for ii,iso in enumerate(isos):
         if iso in el_trans.keys(): ciso = el_trans[iso]
         else:  ciso = '^'+iso[:2]+'^'+iso[2:]
-
+        
         if ciso not in decay_products: 
             continue
         if decay_products[ciso] not in isotope_yield: 
-            isotope_yield[decay_products[ciso]] = np.zeros((len(Zs)),dtype=mass_type)
+            isotope_yield[decay_products[ciso]] = np.zeros((len(Zs),len(masses)))
+        mass_bools = np.zeros(len(masses),dtype='bool')
         for m in prods.dtype.fields.keys():
-            isotope_yield[decay_products[ciso]][m][iz] += prods[m][ii]
+            imass = np.argwhere(int(m[:-1])==masses)[0][0]
+            # take highest energy model available
+            if m[-1] == models[imass]:
+                isotope_yield[decay_products[ciso]][iz,imass] += prods[m][ii]
+                mass_bools[imass]=True
+        # interpolate / extrapolate fields not filled
+        igood = np.argwhere(mass_bools)
+        ibad = np.argwhere(mass_bools==False)
+        if len(ibad) == 0: continue
+        for intm in ibad.T[0]:
+            if masses[intm] > 12:  #interpolate
+                isotope_yield[decay_products[ciso]][iz,intm] = \
+                    np.interp(masses[intm], masses[igood].T[0], 
+                              isotope_yield[decay_products[ciso]][iz,igood].T[0])
+            else: #extrapolate
+                fit = np.polyfit(masses[igood].T[0], 
+                                 isotope_yield[decay_products[ciso]][iz,igood].T[0],2)
+                line = np.poly1d(fit)
+                isotope_yield[decay_products[ciso]][iz,intm] = \
+                    np.max([0,line(masses[intm])])
+
     if 'm_ej' not in isotope_yield: 
-        isotope_yield['m_ej'] = np.zeros((len(Zs)),dtype=mass_type)
+        isotope_yield['m_ej'] = np.zeros((len(Zs),len(masses)))
     for m in prods.dtype.fields.keys():
-        isotope_yield['m_ej'][m][iz] += prods[m][ii-1]
+        if m[-1] == 'A':
+            imass = np.argwhere(int(m[:-1])==masses)[0][0]
+            isotope_yield['m_ej'][iz,imass] = prods[m][ii]
         
-    for iso in isotope_yield.keys():
-        element = iso.split('^')[-1]
-        if iz==0: element_yield[element] = np.zeros((len(Zs)),dtype=mass_type)
-        for m in mass_names:
-            element_yield[element][m][iz] += isotope_yield[iso][m][iz]
-    if 'm_ej' not in element_yield: 
-        element_yield['m_ej'] = np.zeros((len(Zs)),dtype=mass_type)
-    for m in prods.dtype.fields.keys():
-        element_yield['m_ej'][m][iz] += prods[m][ii-1]
+for iso in isotope_yield.keys():
+    if iso =='m_ej': continue
+    element = iso.split('^')[-1]
+    if element not in element_yield: element_yield[element] = np.zeros((len(Zs),len(masses)))
+    element_yield[element] += isotope_yield[iso]
+    
+element_yield['m_ej'] = isotope_yield['m_ej']
 
     
-pdb.set_trace()
 pickle.dump({'isotope_yield':isotope_yield,'element_yield':element_yield,
-             'masses':mass_names,'Zs':np.array(Zs)},open('ww95.pck','w'))
+             'masses':masses,'Zs':np.array(Zs)},open('ww95.pck','w'))
+
+pdb.set_trace()
